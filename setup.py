7153350d7caf1823941400e7908f1fb5c986fd6a
hezronkimutai <hezronchelimo.hc@gmail.com>
2019-01-26 07:49:41 +0300

    Intergrationwith travisci

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='travistest',
+    packages=['travistest'],
+    version='0.0.7',
+    description='Questioner backend',
+    url='https://github.com/hezronkimutai/questioner-backend',    
+)
