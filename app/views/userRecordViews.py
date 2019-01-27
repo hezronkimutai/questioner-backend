9dba82ec819132c2d4156961d365ea11a81cf22b
hezronkimutai <hezronchelimo.hc@gmail.com>
2019-01-27 22:05:22 +0300

    login update

--- app/views/userRecordViews.py
+++ app/views/userRecordViews.py
@@ -13,6 +13,10 @@ user_parser.add_argument('username', type=str, help='Enter your username', requi
 user_parser.add_argument('password', type=str, help='Enter your password', required=True)
 user_parser.add_argument('repeat_password', type=str, help='Repeat your password' , required=True)
 
+login_parser = reqparse.RequestParser()
+login_parser.add_argument('login_email', type=str, help='Please enter the email', required=True)
+login_parser.add_argument('login_password', type=str, help='please enter the password', required=True)
+
 
 class UserRecord(Resource):
     def get(self):
@@ -26,11 +30,24 @@ class UserRecord(Resource):
         'full_name': user_args['full_name'] ,
         'email': user_args['email'],
         'username': user_args['username'] ,
-         'password': user_args['password'],
-         'repeat_password': user_args['repeat_password']
-          }
+        'password': user_args['password'],
+        'repeat_password': user_args['repeat_password']
+        }
         return user, 201
 
+class UserLogin(Resource):
+    def get(self):
+        return {"All_users":"All_users"}
+
+    def post(self):
+        login_args = login_parser.parse_args()
+        '''todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id'''
+        login_user = {
+        'email': login_args['login_email'],
+         'password': login_args['login_password']
+          }
+        return login_user, 201
 
 
 class Singleuser(Resource):
@@ -39,5 +56,8 @@ class Singleuser(Resource):
 
 
 
+api.add_resource(UserLogin, '/login')
+'''api.add_resource(UserLogout, '/logout')
+api.add_resource(ResetPassword, '/reset-password')'''
 api.add_resource(UserRecord, '/users')
 api.add_resource(Singleuser, '/user')
